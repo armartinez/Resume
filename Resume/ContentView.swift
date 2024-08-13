10695c55f224d7f726dd3bb86b9125621d5f1a7e
Axel Martinez <axel.rm@icloud.com>
2024-08-13 11:19:27 +0200

    Initial Commit

---
+++ Resume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resume
+//
+//  Created by Axel Martinez on 13/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Axel Martinez on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
